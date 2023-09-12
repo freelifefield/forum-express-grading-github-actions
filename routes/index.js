89150600a5631596c298ca5fa57975b27af3d16d
EugeneChen <yujiechen0514@gmail.com>
2023-09-12 14:04:21 +0800

    test: test userController

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const passport = require('../config/passport')
 
+const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller');
 const router = express.Router()
 
+const { authenticatedAdmin } = require('../middleware/auth')
 
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
