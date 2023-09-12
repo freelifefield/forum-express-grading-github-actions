60b189ea8566578258a564751bd1bed25023e675
EugeneChen <yujiechen0514@gmail.com>
2023-09-12 14:12:47 +0800

    test: test userController

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
-app.use(passport.initialize())
-app.use(passport.session())
 app.use(flash())
+app.use(passport.initialize())
 
 
 app.listen(port, () => {
