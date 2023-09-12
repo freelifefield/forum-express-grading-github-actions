1f336b3e3a0394b456d1634360e0eda542865eee
EugeneChen <yujiechen0514@gmail.com>
2023-09-12 13:42:09 +0800

    test: test userController

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const routes = require('./routes')
 
+const express = require('express')
+const routes = require('./routes')
+const exhbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+
+
 console.log('hi')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(routes)
+
+const SESSION_SECRET = 'secret'
+
+app.engine('hbs', exhbs({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
