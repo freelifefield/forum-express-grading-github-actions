c800574bb7eab807adb7ac1c5b33d40289dbcfcc
EugeneChen <yujiechen0514@gmail.com>
2023-09-12 13:37:24 +0800

    test: test userController

---
+++ controllers/user-controller.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs')
+
+const db = require('../models')
+const { User } = db
+
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+
+    if (password !== passwordCheck) throw new Error('Password do not match!')
+
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) throw new Error('Email already exist!')
+
+        return bcrypt.hash(password, 10)
+      })
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號!')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  singIn: (req, res) => {
+    req.flash('success_messages', '成功登入!')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '成功登出!')
+    req.logout()
+    res.redirect('/signin')
+  }
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const passport = require('../config/passport')
+
+const userController = require('../controllers/user-controller');
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.singIn)
+router.get('/logout', userController.logout)
 
 module.exports = router
