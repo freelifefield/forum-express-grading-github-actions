c496dbb809d257d5616101abf6c17b96f27f4af8
EugeneChen <yujiechen0514@gmail.com>
2023-10-03 22:16:07 +0800

    test: add prod code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const passport = require('../config/passport')
+
+const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller');
+
+
+const { authenticatedAdmin } = require('../middleware/auth')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.use('/admin', authenticatedAdmin, admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.singIn)
+router.get('/logout', userController.logout)
 
 module.exports = router
